fcdb4f73ff6679387f94ebd691100d35371fe72d
Angiecodes <angelcoder87@gmail.com>
2023-06-23 02:12:38 +0300

    New changes

--- pall.c
+++ pall.c
@@ -1,39 +1,19 @@
 #include "monty.h"
-/**
- * _push - pushes an element to the stack
- * @stack: pointer to the top of the stack
- * @line_number: the index of the current line
- */
-void _push(stack_t **stack, unsigned int line_number)
-{
-	char *arg = strtok(NULL, " \t\n");
-	int num;
-
-	if (!arg || !isdigit(*arg))
-	{
-	fprintf(stderr, "L%d: usage: push integer\n", line_number);
-	exit(EXIT_FAILURE);
-	}
-
-	num = atoi(arg);
-	push(stack, num);
-}
 
 /**
  * _pall - prints all the values on the stack
- * @stack: pointer to the top of the stack
- * @line_number: the index of the current line
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number of the opcode in the Monty file
  */
 void _pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
+	stack_t *current = *stack;
 
-	(void)line_number;
-
-	while (tmp)
+	while (current != NULL)
 	{
-	printf("%d\n", tmp->n);
-	tmp = tmp->next;
+	printf("%d\n", current->n);
+	current = current->next;
 	}
 }
+
 
